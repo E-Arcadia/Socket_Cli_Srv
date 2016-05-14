152d37d58e39099c13df87c3331cfce305106869
unknown <978907@l07m00.acad.unibrasil.com.br>
2016-05-13 21:04:53 -0300

    Interface cliente em Swing concluída.

--- src/View/ClienteSwing.java
+++ src/View/ClienteSwing.java
@@ -28,9 +28,9 @@ import Util.Pacote.indicativo;
 public class ClienteSwing extends JFrame implements ActionListener, InterfaceCliente {
 	private SimpleDateFormat dhf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	private Rede umaRede = null;
-	JTextField jtfIP, jtfPorta, jtfMensagem;
+	JTextField jtfIP, jtfPorta, jtfMensagem, jtfNome, jtfEMail;
 	JButton btConectar, btSair;
-	JPanel panelConexao, panelComunicacao, panelRetorno, panelMensagem;
+	JPanel painelConexao, painelComunicacao, painelRetorno, painelMensagem, painelCadastraPessoa, NoCENTRO ;
 	JTextArea txtArea;
 	Boolean mensagemVisivil = false;
 	Pacote novoPacote = null;
@@ -46,109 +46,143 @@ public class ClienteSwing extends JFrame implements ActionListener, InterfaceCli
 		jtfTitulo.setFont(new Font(null, Font.BOLD, 22));
 		add(jtfTitulo, BorderLayout.NORTH);
 
-		add(jpConexao(), BorderLayout.CENTER);
+		alteraPainelCentro(jpConexao());
 		setVisible(true);
 	}
 
 	// JPanel Conexão
 	private JPanel jpConexao() {
-		panelConexao = new JPanel();
-		panelConexao.setLayout(new GridLayout(3, 2));
+		painelConexao = new JPanel();
+		painelConexao.setLayout(new GridLayout(3, 2));
 
 		// Endereço IP
-		panelConexao.add(new JLabel("Endereço do Servidor"));
+		painelConexao.add(new JLabel("Endereço do Servidor"));
 		jtfIP = new JTextField("127.0.0.1");
-		panelConexao.add(jtfIP);
+		painelConexao.add(jtfIP);
 
 		// PORTA
-		panelConexao.add(new JLabel("Porta no Servidor"));
+		painelConexao.add(new JLabel("Porta no Servidor"));
 		jtfPorta = new JTextField("12345");
-		panelConexao.add(jtfPorta);
+		painelConexao.add(jtfPorta);
 
 		btConectar = new JButton("Conectar");
 		btConectar.setActionCommand("CONECTAR");
 		btConectar.addActionListener(this);
-		panelConexao.add(btConectar);
+		painelConexao.add(btConectar);
 
 		btSair = new JButton("Sair");
 		btSair.setActionCommand("SAIR");
 		btSair.addActionListener(this);
-		panelConexao.add(btSair);
+		painelConexao.add(btSair);
 
-		return panelConexao;
+		return painelConexao;
 	}
 
 	// JPanel Comunicação
 	private JPanel jpComunicacao() {
-		panelComunicacao = new JPanel();
-		panelComunicacao.setLayout(new GridLayout(8, 1));
+		painelComunicacao = new JPanel();
+		painelComunicacao.setLayout(new GridLayout(8, 1));
 
 		JButton jbMensagem = new JButton("Envia Mensagem");
 		jbMensagem.setActionCommand("MENSAGEM");
 		jbMensagem.addActionListener(this);
-		panelComunicacao.add(jbMensagem);
+		painelComunicacao.add(jbMensagem);
 
 		JButton jbData = new JButton("Data do Servidor");
 		jbData.setActionCommand("DATA");
 		jbData.addActionListener(this);
-		panelComunicacao.add(jbData);
+		painelComunicacao.add(jbData);
 
 		JButton jbHora = new JButton("Hora do Servidor");
 		jbHora.setActionCommand("HORA");
 		jbHora.addActionListener(this);
-		panelComunicacao.add(jbHora);
+		painelComunicacao.add(jbHora);
 
 		JButton jbInsere = new JButton("Insere um Registro");
 		jbInsere.setActionCommand("INSERE");
 		jbInsere.addActionListener(this);
-		panelComunicacao.add(jbInsere);
+		painelComunicacao.add(jbInsere);
 
 		JButton jbListar = new JButton("Listar no Servidor");
 		jbListar.setActionCommand("LISTAR");
 		jbListar.addActionListener(this);
-		panelComunicacao.add(jbListar);
+		painelComunicacao.add(jbListar);
 
 		JButton jbListarLocal = new JButton("Listar Local");
 		jbListarLocal.setActionCommand("LISTAR_LOCAL");
 		jbListarLocal.addActionListener(this);
-		panelComunicacao.add(jbListarLocal);
+		painelComunicacao.add(jbListarLocal);
 
 		JButton jbEncerra = new JButton("Encerra Servidor");
 		jbEncerra.setActionCommand("ENCERRA");
 		jbEncerra.addActionListener(this);
-		panelComunicacao.add(jbEncerra);
+		painelComunicacao.add(jbEncerra);
 
 		JButton jbDesconecta = new JButton("Desconecta");
 		jbDesconecta.setActionCommand("DESCONECTA");
 		jbDesconecta.addActionListener(this);
-		panelComunicacao.add(jbDesconecta);
+		painelComunicacao.add(jbDesconecta);
 
-		return panelComunicacao;
+		return painelComunicacao;
 	}
 
 	// JScrollPane Texto de Retorno
 	private JPanel jpRetorno() {
-		panelRetorno = new JPanel();
-		panelRetorno.setLayout(new FlowLayout());
-		txtArea = new JTextArea(20, 30);
-		JScrollPane scroll = new JScrollPane(txtArea);
-		panelRetorno.add(scroll);
-
-		return panelRetorno;
+		if(painelRetorno == null){
+			painelRetorno = new JPanel();
+			painelRetorno.setLayout(new FlowLayout());
+			txtArea = new JTextArea(20, 30);
+			JScrollPane scroll = new JScrollPane(txtArea);
+			painelRetorno.add(scroll);
+		}
+		return painelRetorno;
 	}
 
 	// JPanel Mensagem
 	private JPanel jpMensagem() {
-		panelMensagem = new JPanel();
-		panelMensagem.setLayout(new GridLayout(1, 2));
+		painelMensagem = new JPanel();
+		painelMensagem.setLayout(new GridLayout(1, 2));
 		jtfMensagem = new JTextField(60);
-		panelMensagem.add(jtfMensagem);
+		painelMensagem.add(jtfMensagem);
 
 		JButton jbMensagem = new JButton("ENVIAR");
 		jbMensagem.setActionCommand("ENVIAR");
 		jbMensagem.addActionListener(this);
-		panelMensagem.add(jbMensagem);
-		return panelMensagem;
+		painelMensagem.add(jbMensagem);
+		return painelMensagem;
+	}
+	
+	//JPanel Cadastra Cliente
+	private JPanel jpCadastraPessoa(){
+		painelCadastraPessoa = new JPanel();
+		painelCadastraPessoa.setLayout(new GridLayout(3, 2));
+		
+		painelCadastraPessoa.add(new JLabel("Nome: "));
+		jtfNome = new JTextField(20);
+		painelCadastraPessoa.add(jtfNome);
+		
+		painelCadastraPessoa.add(new JLabel("E-Mail: "));
+		jtfEMail = new JTextField(20);
+		painelCadastraPessoa.add(jtfEMail);
+		
+		painelCadastraPessoa.add(new JLabel(""));
+		
+		JButton jbSalvar = new JButton("SALVAR");
+		jbSalvar.setActionCommand("SALVAR");
+		jbSalvar.addActionListener(this);
+		painelCadastraPessoa.add(jbSalvar);
+		return painelCadastraPessoa;
+	}
+	
+	private void alteraPainelCentro(JPanel jp){
+		if(!(jp == painelRetorno && NoCENTRO == painelRetorno)){
+			if(NoCENTRO != null){
+				remove(NoCENTRO);
+			}
+			NoCENTRO = jp;
+			add(jp, BorderLayout.CENTER);
+			setVisible(true);
+		}
 	}
 
 	@Override
@@ -163,8 +197,7 @@ public class ClienteSwing extends JFrame implements ActionListener, InterfaceCli
 				JOptionPane.showMessageDialog(this, "Conectado ao Servidor");
 				btConectar.setEnabled(false);
 				add(jpComunicacao(), BorderLayout.WEST);
-				remove(panelConexao);
-				add(jpRetorno(), BorderLayout.CENTER);
+				alteraPainelCentro(jpRetorno());
 				setSize(500, 400);
 				setVisible(true);
 			} else {
@@ -174,9 +207,10 @@ public class ClienteSwing extends JFrame implements ActionListener, InterfaceCli
 
 		case "MENSAGEM":
 			if (mensagemVisivil) {
-				remove(panelMensagem);
+				remove(painelMensagem);
 				mensagemVisivil = false;
 			} else {
+				alteraPainelCentro(jpRetorno());
 				add(jpMensagem(), BorderLayout.SOUTH);
 				jtfMensagem.requestFocus();
 				mensagemVisivil = true;
@@ -184,26 +218,39 @@ public class ClienteSwing extends JFrame implements ActionListener, InterfaceCli
 			setVisible(true);
 			break;
 		case "DATA":
+			alteraPainelCentro(jpRetorno());
 			novoPacote = new Pacote(indicativo.DATA, "");
 			if (!umaRede.envia(novoPacote)) {
 				JOptionPane.showMessageDialog(this, "Erro de envio");
 			}
 			break;
 		case "HORA":
+			alteraPainelCentro(jpRetorno());
 			novoPacote = new Pacote(indicativo.HORA, "");
 			if (!umaRede.envia(novoPacote)) {
 				JOptionPane.showMessageDialog(this, "Erro de envio");
 			}
 			break;
 		case "INSERE":
+			alteraPainelCentro(jpCadastraPessoa());
+			jtfNome.requestFocus();
+			break;
+		case "SALVAR":
+			Pessoa umaPessoa = new Pessoa(null, jtfNome.getText(), jtfEMail.getText());
+			novoPacote = new Pacote(indicativo.INSERE, umaPessoa);
+			if (!umaRede.envia(novoPacote)) {
+				JOptionPane.showMessageDialog(this, "Erro de envio");
+			}
 			break;
 		case "LISTAR":
+			alteraPainelCentro(jpRetorno());
 			novoPacote = new Pacote(indicativo.LISTA, "");
 			if (!umaRede.envia(novoPacote)) {
 				JOptionPane.showMessageDialog(this, "Erro de envio");
 			}
 			break;
 		case "LISTAR_LOCAL":
+			alteraPainelCentro(jpRetorno());
 			novoPacote = new Pacote(indicativo.LISTA_LOCAL, "");
 			if (!umaRede.envia(novoPacote)) {
 				JOptionPane.showMessageDialog(this, "Erro de envio");
@@ -250,6 +297,9 @@ public class ClienteSwing extends JFrame implements ActionListener, InterfaceCli
 			break;
 		case "INSERE":
 			txtArea.append("\nRESULTADO: " + (String) p.getObj());
+			jtfNome.setText("");
+			jtfEMail.setText("");
+			jtfNome.requestFocus();
 			break;
 		case "LISTA":
 			txtArea.append("\nRESULTADO: " + (String) p.getObj());
@@ -259,28 +309,28 @@ public class ClienteSwing extends JFrame implements ActionListener, InterfaceCli
 			listaPessoas = (ArrayList<Pessoa>) p.getObj();
 			txtArea.append("\nListando o Recebido");
 			for (Pessoa umaPessoa : listaPessoas) {
-				txtArea.append(umaPessoa.toString());
+				txtArea.append("\n"+umaPessoa.toString());
 			}
 			break;
-		
 		case "ENCERRA":
 			txtArea.append("\nRESULTADO: " + (String) p.getObj());
 		case "DESCONECTA":
 			umaRede.desconectar();
-			remove(panelRetorno);
-			add(panelConexao);
+			alteraPainelCentro(jpConexao());
 			btConectar.setEnabled(true);
-			remove(panelComunicacao);
+			remove(painelComunicacao);
 			setSize(400, 200);
 			setVisible(true);
 
 			// Retira mensagem
 			if (mensagemVisivil) {
-				remove(panelMensagem);
+				remove(painelMensagem);
 				mensagemVisivil = false;
 			}
 			break;
 		}
 
 	}
+	
+	
 }
